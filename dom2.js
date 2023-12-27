d4616d006f7a894cc0d2b205c5af07bb017b823a
abhay <zzxxcc6386@gmail.com>
2023-12-27 16:17:15 +0530

    dom

---
+++ dom2.js
@@ -0,0 +1,42 @@
+// console.log("aa");
+
+// DOm Mouplation all commands for brawser
+
+// document.getElementById('tit').className
+
+// document.getElementById('ulist')​
+// document.getElementById('ulist').getAttribute('id')
+// document.getElementById('ulist').setAttribute('class' , 'list')
+// document.getElementById('ulist').getAttribute('class')
+
+
+
+// document.getElementById('id1')
+// const aaa = document.getElementById('id1')
+// aaa.style.backgroundColor = 'green'
+
+
+
+// const aa = document.getElementById('id1')
+// aa.textContent
+// aa.innerHTML
+// aa.style.backgroundColor = green
+// aa.style.backgroundColor = "green"
+// aa.style.padding = "10px"
+// aa.id
+// aa.className
+// aa.innerHTML
+// aa.getInnerHTML
+
+
+
+// **************** queryselector / queryselectorAoll ************
+
+// const aa = document.getElementById('queryid')
+// // undefined   //***** output  */
+// aa
+// // <div class=​"queryclass" id=​"queryid">​…​</div>​   //***** output  */
+// aa.querySelector('h1')
+// // <h1 class=​"headingquery">​This is heading 1 by Queryselector​</h1>​   //***** output  */
+// aa.querySelectorAll('h1')
+// // NodeList(4) [h1.headingquery, h1.headingquery, h1.headingquery, h1.headingquery]   //***** output  */
